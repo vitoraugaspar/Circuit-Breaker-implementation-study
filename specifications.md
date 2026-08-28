0d45b95f778628c293f06a506080d4f02ce0c84b
vitoraugaspar <vitor.gaspar@fatec.sp.gov.br>
2026-08-28 10:19:57 -0300

    feat: add OPEN state time mechanic.

--- specifications.md
+++ specifications.md
@@ -1,8 +1,8 @@
 **Especificações**
 - CircuitBreaker por natureza está fechado [x]
 - Passa transações no estado fechado até atingir o limite permitido [x]
-- Quando atingido, entra em open por 30s []
-- Depois desse tempo, vai para o estado half-open []
+- Quando atingido, entra em open por 30s [x]
+- Depois desse tempo, vai para o estado half-open [x]
 - Guarda a primeira transacao que chega em half-open em cache e utiliza para testa-la e depois descarta []
 - Caso falhe, retorna para open []
 - Caso dê certo, vai para fechado []
--- src/CircuitBreaker.java
+++ src/CircuitBreaker.java
@@ -4,6 +4,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class CircuitBreaker {
     public CircuitBreaker(Short newLimit){
@@ -19,6 +22,7 @@ public class CircuitBreaker {
     private final HttpClient client = HttpClient.newBuilder()
             .connectTimeout(Duration.ofSeconds(5))
             .build();
+    private final ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
 
        public void calls(String uri, String body){
            if(getState() == State.CLOSED){
@@ -35,8 +39,9 @@ public class CircuitBreaker {
                        transactionsFailed++;
                        if (transactionsFailed >= limitPermittedForFailTransactions){
                            state = State.OPEN;
-                           timeStamp = LocalTime.now();
                            transactionsFailed = 0;
+                           Runnable runnable = () -> setState(State.HALF_OPEN);
+                           executor.schedule(runnable, 30, TimeUnit.SECONDS);
                        }
                        throw new RuntimeException("Erro ao mandar a requisição");
                    }
@@ -48,8 +53,9 @@ public class CircuitBreaker {
                    transactionsFailed++;
                    if (transactionsFailed >= limitPermittedForFailTransactions) {
                        state = State.OPEN;
-                       timeStamp = LocalTime.now();
                        transactionsFailed = 0;
+                       Runnable runnable = () -> setState(State.HALF_OPEN);
+                       executor.schedule(runnable, 30, TimeUnit.SECONDS);
                    }
                    throw new RuntimeException("Requisição falhou");
                }
@@ -59,10 +65,15 @@ public class CircuitBreaker {
 
            }
            if(getState() == State.OPEN){
-
+               throw new RuntimeException("Requisição falhou");
            }
        }
         public State getState() {
             return state;
         }
+
+    public void setState(State state){
+        this.state = state;
     }
+    }
+
